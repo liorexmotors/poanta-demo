32cd3d330b68e73dbcfd1ef960b58b2d9404317e
root <root@vmi3259678.contaboserver.net>
2026-05-17 10:30:47 +0200

    Add Poanta preview studio and exact feed layout

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'poanta-demo-v22';
+const CACHE_NAME = 'poanta-demo-v23-layout';
 const ASSETS = ['./', './index.html', './manifest.webmanifest'];
 
 self.addEventListener('install', event => {
