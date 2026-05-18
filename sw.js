41c10d3c4965e63b9866d57cfab365db0a7aed49
root <root@vmi3259678.contaboserver.net>
2026-05-18 10:08:19 +0200

    Soften read article styling

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'poanta-demo-v30-single-date-sort';
+const CACHE_NAME = 'poanta-demo-v31-soft-read-state';
 const ASSETS = ['./', './index.html', './manifest.webmanifest'];
 
 self.addEventListener('install', event => {
