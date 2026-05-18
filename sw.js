ab1b99540198b78da04a8a51e795686dcd0c3dc8
root <root@vmi3259678.contaboserver.net>
2026-05-18 09:57:16 +0200

    Add sort icon and improve image placeholders

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'poanta-demo-v28-date-time-sort';
+const CACHE_NAME = 'poanta-demo-v29-sort-icon-placeholder';
 const ASSETS = ['./', './index.html', './manifest.webmanifest'];
 
 self.addEventListener('install', event => {
