81e621ac9d1e1c9c95630baec7c7a39f474050b7
root <root@vmi3259678.contaboserver.net>
2026-05-18 10:09:55 +0200

    Deploy Poanta chronological sort and soft read state

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'poanta-demo-v29-sort-icon-placeholder';
+const CACHE_NAME = 'poanta-demo-v31-soft-read-state';
 const ASSETS = ['./', './index.html', './manifest.webmanifest'];
 
 self.addEventListener('install', event => {
